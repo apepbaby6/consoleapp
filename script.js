22a6ceca6c16bd6b5bfacd86d9d3ea6a87de44cd
Andre <apepbaby147@gmail.com>
2021-04-08 20:40:22 +0500

    consoleapp commit 2

--- script.js
+++ script.js
@@ -10,12 +10,57 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }   
+// }
 
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+let i = 0;
+
+// do {
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         i--;
+//         console.log('error');
+//     }
+// } while (i < 2);
+
+while (i < 2) {
+    i++;
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('error');
+    }
+}
 console.log(personalMovieDB);
